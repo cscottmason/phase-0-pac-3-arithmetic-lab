8fd1fa6ef6de0203d5159ff4dafaf6a15ad735a6
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-13 18:11:55 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+a = 10 * 1000
+b = 5 * 1000
+
+function add(a, b){
+
+    return (a + b)
+}
+
+
+function subtract(a,b){
+    return (a - b)
+
+}
+
+function multiply(a,b){
+    return (a * b)
+
+}
+
+function divide(a,b){
+    return (a / b)
+
+}
+
+function increment(n){
+    return (n + 1)
+}
+
+function decrement(n){
+    return (n - 1)
+}
+
+function makeInt(n){
+   return parseInt (n);
+   
+}
+
+
+function makeInt(n){
+return parseInt (n, 10)}
+    
+
+function returnsNaNAsAppropriate(){
+    return parseInt ("sldkjflksjf")
+}
+
+
+function preserveDecimal(n){
+    return parseFloat(n)
+
+}
+
+function returnsNaNAsAppropriate(){
+    return parseFloat("sldkjflksjf")
+}
